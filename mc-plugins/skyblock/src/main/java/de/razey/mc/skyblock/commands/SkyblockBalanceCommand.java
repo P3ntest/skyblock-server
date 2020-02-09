2bd86eb1e2eac6679ef2034e403b9ef37697b320
xXraxFraeyXx <juliusvanvoorden@outlook.com>
2020-02-09 18:39:23 +0100

    add Balance Command

--- mc-plugins/skyblock/src/main/java/de/razey/mc/skyblock/commands/SkyblockBalanceCommand.java
+++ mc-plugins/skyblock/src/main/java/de/razey/mc/skyblock/commands/SkyblockBalanceCommand.java
@@ -1,12 +1,25 @@
 package de.razey.mc.skyblock.commands;
 
+import de.razey.mc.core.api.CoreApi;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class SkyblockBalanceCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+
+        if (args.length == 0) {
+            if (!(sender instanceof Player))
+                return true;
+
+            Player player = (Player) sender;
+
+            CoreApi.getInstance().displayMessage(player, "skyblock.balance.display", "skyblock",
+                    "" + CoreApi.getInstance().getSkyblockBalanceFromPlayerId(CoreApi.getInstance().getPlayerId(player.getUniqueId().toString())));
+        }
+
         return false;
     }
 }
