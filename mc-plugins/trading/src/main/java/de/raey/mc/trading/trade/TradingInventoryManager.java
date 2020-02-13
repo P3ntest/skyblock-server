185c3408e80d0b74d92c3e68f29b7abe9d29811b
xXraxFraeyXx <juliusvanvoorden@outlook.com>
2020-02-13 20:49:11 +0100

    add new hashmap

--- mc-plugins/trading/src/main/java/de/raey/mc/trading/trade/TradingInventoryManager.java
+++ mc-plugins/trading/src/main/java/de/raey/mc/trading/trade/TradingInventoryManager.java
@@ -3,11 +3,14 @@ package de.raey.mc.trading.trade;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public abstract class TradingInventoryManager {
 
     public static List<Trade> trades = new ArrayList<>();
+
+    public static HashMap<Player, Player> tradeRequests;
 
     public static Trade getTradePlayerIsIn(Player p) {
         for (Trade trade : trades) {
