1e967ffed54afd2b96b5bc251492e7d1bf879077
RobinD95 <lopfi95@gmail.com>
2020-02-13 21:39:05 +0100

    fix EntityDamageEvent again

--- mc-plugins/skyblock/src/main/java/de/razey/mc/skyblock/events/SkyblockEntityDamageEvent.java
+++ mc-plugins/skyblock/src/main/java/de/razey/mc/skyblock/events/SkyblockEntityDamageEvent.java
@@ -13,9 +13,9 @@ public class SkyblockEntityDamageEvent implements Listener {
         if (e.getEntity() instanceof Player) {
             Player player = (Player) e.getEntity();
             if (e.getCause() == EntityDamageEvent.DamageCause.FALL && e.getDamage() >= player.getHealth()) {
-                e.setDamage(0);
-                e.setCancelled(true);
-                player.setHealth(1);
+                e.setDamage(player.getHealth()-1);
+                //e.setCancelled(true);
+                //player.setHealth(1);
             }
         }
     }
